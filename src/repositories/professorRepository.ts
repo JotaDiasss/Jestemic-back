49a2ecea4c9a02239b7d11929b2fb57adebab26f
jotadiasss <joaopefdias@gmail.com>
2026-06-10 16:53:38 -0300

    inciando professorRepository

--- src/repositories/professorRepository.ts
+++ src/repositories/professorRepository.ts
@@ -3,5 +3,39 @@ import { professors } from "../database/postgres/schemas/professors.js"
 import { eq } from "drizzle-orm"
 
 export async function findAllProfessors() {
-    const result = await postgresDB.select().from(professors)
+    const result = await postgresDB
+        .select()
+        .from(professors)
+    return result
+}
+
+export async function findProfessorById(id: number) {
+    const result = await postgresDB
+        .select()
+        .from(professors)
+        .where(eq(professors.id, id))
+    return result[0]
+}
+
+export async function createProfessor(data: { name: string }) {
+    const result = await postgresDB
+        .insert(professors)
+        .values(data)
+        .returning()
+    return result[0]
+}
+
+export async function updateProfessor(id: number, data: { name?: string }) {
+    const result = await postgresDB
+        .update(professors)
+        .set(data)
+        .where(eq(professors.id, id))
+        .returning()
+    return result[0]
+}
+
+export async function deleteProfessor(id: number) {
+    await postgresDB
+        .delete(professors)
+        .where(eq(professors.id, id))
 }
